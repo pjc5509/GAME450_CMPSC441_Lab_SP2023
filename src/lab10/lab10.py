c1ba38904080a1711639b4c67fd6b586c016699f
pjc5509 <pjc5509@psu.edu>
2023-03-17 09:48:28 -0400

    Lab10 Work in progress

--- src/lab10/lab10.py
+++ src/lab10/lab10.py
@@ -10,13 +10,15 @@ from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("src/lab8/heart.csv")
+#import data set
+data = pd.read_csv("src/lab10/heart.csv")
 
 # Transform the categorical variables into dummy variables.
 print(data.head())
 string_col = data.select_dtypes(include="object").columns
 df = pd.get_dummies(data, columns=string_col, drop_first=False)
-print(data.head())
+print(df.head())
+
 
 y = df.HeartDisease.values
 x = df.drop(["HeartDisease"], axis=1)
@@ -26,12 +28,28 @@ x_train, x_test, y_train, y_test = train_test_split(
 
 """ Train a sklearn model here. """
 
+#sklearn_model = None
+
 sklearn_model = None
+
+sklearn_model = KNeighborsClassifier(n_neighbors=3)
+sklearn_model.fit(x_train, y_train)
+
+
+
+
+
+
 
 # Accuracy
 print("Accuracy of model: {}\n".format(sklearn_model.score(x_test, y_test)))
 
 
 """ Improve the model by normalizing the input data. """
+#make all data 0-1 so if 0-10 maybey do >5 
+#val - min / max - min
+# ex 10 to 5 , 10 - 5 / 10 - 5  = 1, 5 - 5/ 10 -5 = 0
+
+
 
 print("Accuracy of improved model: {}\n".format(sklearn_model.score(x_test, y_test)))
